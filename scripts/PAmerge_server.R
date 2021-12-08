3a3e04c886124a3c3fe5a249968a7bee239cbffe
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2021-12-08 18:18:23 +1300

    accounting for different permit hours, event types, and other adjustments for a more complicated report

--- scripts/PAmerge_server.R
+++ scripts/PAmerge_server.R
@@ -52,9 +52,11 @@ observeEvent(input$photogo,{
   daterange<-range(sigs$Datetime)
   
   raw_tracks<-sf::st_read(paste0(pathimage,"/Tracks"), layer = paste0(phyear,"_",phmonth), quiet = T)
+
   tracks<-as.data.frame(raw_tracks)%>%
     dplyr::select(DATE, TIME, LAT, LON)%>%
     mutate(Datetime = ymd_hms(paste(DATE, TIME)))%>%
+    #filter(Datetime >= ymd_hms('2021-09-30 10:59:00') & Datetime <= ymd_hms('2021-09-30 11:20:00'))%>%
     dplyr::select(Datetime, everything())%>%
     arrange(Datetime)%>%
     distinct()%>%
@@ -71,34 +73,50 @@ observeEvent(input$photogo,{
   #filter tracks to only between on/off effort      
   f_data<-tracks%>%
     full_join(sigs, by = c("Datetime","LAT"="Lat","LON"="Lon","DATE"="Date","TIME"="Time"))%>%
-    mutate(DATE = as.factor(DATE))%>%
+    mutate(DATE = as.factor(DATE),
+           across(where(is.character), ~na_if(., "")))%>% # add in NAs where there are blanks
+    #group_by(DATE)%>%
+    #mutate(Event = case_when(Effort == 'Effort ON' ~ 1, TRUE ~ 0))%>%
+    #ungroup()%>%
     arrange(Datetime)%>%
     mutate(Effort = case_when(
-      Home.screen == "Encounter START" & grepl('FOLLOW$', toupper(Note)) ~ 'Follow ON',
-      Home.screen == "Encounter END & DATA" & grepl('FOLLOW$', toupper(Note)) ~ 'Follow OFF',
-      Home.screen == "Encounter START" & grepl('REPEAT$', toupper(Note)) ~ 'Repeat Encounter ON',
-      Home.screen == "Encounter END & DATA" & grepl('REPEAT$', toupper(Note)) ~ 'Repeat Encounter OFF',
+      Home.screen == "Encounter START" & grepl('^FOLLOW', toupper(Note)) ~ 'Follow ON',
+      Home.screen == "Encounter END & DATA" & grepl('^FOLLOW', toupper(Note)) ~ 'Follow OFF',
+      Home.screen == "Encounter START" & grepl('^REPEAT', toupper(Note)) ~ 'Repeat Encounter ON',
+      Home.screen == "Encounter END & DATA" & grepl('^REPEAT', toupper(Note)) ~ 'Repeat Encounter OFF',
       Home.screen == "Encounter START" ~ 'Encounter ON',
       Home.screen == "Encounter END & DATA" ~ 'Encounter OFF',
       TRUE ~ Effort
+    ),
+    Permit = case_when(
+      grepl('^DOC', toupper(Note)) ~ 'DOC',
+      grepl('^UO', toupper(Note)) ~ 'UO',
+      TRUE ~ ''
     ))
+  
+  Event<-f_data%>%filter(Effort == "Effort ON")%>%ungroup()%>%mutate(Event = 1:n())%>%as.data.frame()
   
   onoffeffort<-f_data%>%
     filter(grepl('Effort',Effort))%>%
     group_by(DATE)%>%
     mutate(min = min(Datetime),
-      max = max(Datetime))%>%
-    distinct(DATE, min, max)%>%
+      max = max(Datetime),
+      permit = Permit)%>%
+    distinct(DATE, min, max, permit)%>%
+    filter(nchar(permit) > 0)%>%
     as.data.frame()
   
   f_data<-f_data%>%
     left_join(onoffeffort, by = 'DATE')%>%
-    filter(Datetime >= min & Datetime <= max)
+    filter(Datetime >= min & Datetime <= max)%>%
+    left_join(Event)
+  
+  f_data$Event<-zoo::na.locf(f_data$Event, na.rm = FALSE)
   
   sig_num<-f_data%>%
     filter(grepl("Encounter", Effort) | grepl("Follow", Effort))%>%
     mutate(signum = rep(1:(n()/2), each = 2))%>%
-    dplyr::select(Datetime, DATE, signum, Home.screen, Effort)
+    dplyr::select(Datetime, DATE, signum, Home.screen, Effort, permit)
   
   f_data$Effort[f_data$Effort==""] <- NA
   
@@ -109,11 +127,10 @@ observeEvent(input$photogo,{
     mutate(Effort = replace(Effort, first(Effort) == 'Repeat Encounter ON', 'Repeat Encounter ON')) %>% 
     mutate(Effort = replace(Effort, first(Effort) == 'Follow ON', 'Follow ON')) %>% 
     ungroup() %>%
-    select(-grp)%>%
     dplyr::select(Datetime, DATE, TIME, LAT, LON, Effort, Crew, signum,
                   Species.encountered, Group.size, No..of.calves, Behaviour.state,
-                  Beaufort, Swell, Sighting.conditions, SST, Depth, Note)
-  
+                  Beaufort, Swell, Sighting.conditions, SST, Depth, Note, Event, -grp)
+     
   
   sig_count<-max(f_data$signum, na.rm = T)
   
@@ -128,24 +145,31 @@ observeEvent(input$photogo,{
   m_nm<-1/1852
   
   f_data_dist<-f_data%>%
-    group_by(DATE)%>%
+    arrange(Datetime)%>%
+    group_by(DATE,Event)%>%
     mutate(LAT2 = dplyr::lag(LAT),
            LON2 = dplyr::lag(LON),
            dist_km = geosphere::distVincentyEllipsoid(matrix(c(LON,LAT), ncol = 2),matrix(c(LON2, LAT2), ncol =2),a=6378137, f=1/298.257222101)*m_nm)
   
+  print(f_data_dist%>%filter(dist_km > 0.1)%>%as.data.frame())
   track_dist<-round(sum(f_data_dist$dist_km, na.rm=TRUE),0)
   
   onoffsigs<-sig_num%>%
-    dplyr::select(signum, Datetime, Effort)%>%
+    dplyr::select(signum, Datetime, Effort, permit)%>%
     group_by(signum)%>%
     tidyr::pivot_wider(names_from = Effort, values_from = Datetime)
   
   hours_wTt<-onoffsigs%>%
-    mutate(time_wTt = as.numeric(`Encounter OFF` - `Encounter ON`),
-           time_follow = as.numeric(`Follow OFF` - `Follow ON`))%>%
+    mutate(time_wTt = as.numeric(difftime(`Encounter OFF`, `Encounter ON`, units = "mins")),
+           time_wTt_r = as.numeric(difftime(`Repeat Encounter OFF`, `Repeat Encounter ON`, units = "mins")),
+           time_follow = as.numeric(difftime(`Follow OFF`, `Follow ON`, units = "mins")))%>%
     ungroup()%>%
-    dplyr::summarise(total_wTt = round(sum(time_wTt, na.rm=TRUE)/60,1),
-                     total_follow = round(sum(time_follow, na.rm=TRUE)/60,1))
+    group_by(permit)%>%
+    dplyr::summarise(total_wTt_e = round(sum(time_wTt, na.rm=TRUE)/60,1),
+                     total_wTt_r = round(sum(time_wTt_r, na.rm=TRUE)/60,1),
+                     total_follow = round(sum(time_follow, na.rm=TRUE)/60,1))%>%
+    mutate(total_wTt = total_wTt_e + total_wTt_r)%>%
+    dplyr::select(-total_wTt_e, -total_wTt_r)
   
   ####################
   ## Photo analysis ##
@@ -177,7 +201,7 @@ observeEvent(input$photogo,{
   folder.list<-list.files(pathimage, pattern = paste0("^",phyear), full.names = T)
   filenames<-sapply(folder.list, function (x) list.files(x, pattern = "*.JPG$|*.jpg$", full.names = T, recursive = T))
   filenames_unlist<-unlist(filenames, use.names = F)
-
+  
   allphotod_df<-data.frame(fullfilename = filenames_unlist,
              filename = basename(filenames_unlist),
              date = ymd(str_extract(filenames_unlist,'\\b\\d{8}\\b')))
@@ -193,11 +217,14 @@ observeEvent(input$photogo,{
     #and print the table
   }
   
+allmerge%>%filter(Date == '2021-10-02')%>%filter(Filename == 'DSC_3171')
+
   files_for_exif<-allphotod_df%>%
     mutate(filename = stringr::str_sub(filename, end = -5))%>%
     right_join(allmerge, by = c("filename" = "Filename", "date" = "Date"))%>%
     distinct(fullfilename)%>%
     filter(!is.na(fullfilename))
+
   incProgress(1/5)
   print("getting exif data")
   #get exif data
@@ -212,12 +239,10 @@ observeEvent(input$photogo,{
   
   allmerge_dt<-allmerge%>%
     left_join(metadata2, by = c("Filename", "Date"))%>%
-    mutate(ID_Name = stringr::str_replace(ID_Name, "JOLLY", "JOLLY-GOOD"),
-           ID_Name = case_when(grepl("EEK", ID_Name) ~ "EEK-THE-CAT",
+    mutate(ID_Name = case_when(grepl("EEK", ID_Name) ~ "EEK-THE-CAT",
+                               grepl("JOLLY", ID_Name ~ "JOLLY-GOOD"),
                                TRUE ~ ID_Name),
            Survey_Area = toupper(pharea))
-
-  write.csv(allmerge_dt, paste0(pathimage,"/Photo analysis/f_PA_",phyear,"_",phmonth,".csv"), row.names = F, na = "")
   
   incProgress(2/5)
   
@@ -227,28 +252,54 @@ observeEvent(input$photogo,{
     allmerge_dt<-read.csv(paste0(pathimage,"/Photo analysis/f_PA_",phyear,"_",phmonth,".csv"), header = T, stringsAsFactors = T)
     allmerge_dt$Datetime<-ymd_hms(allmerge_dt$Datetime)
     allmerge_dt$Date<-ymd(allmerge_dt$Date)
-    head(allmerge_dt)
+
     }
   
-  tripdate_s_dt<-format(min(as.Date(allmerge_dt$Date)), "%d %b %Y")
-  tripdate_e_dt<-format(max(as.Date(allmerge_dt$Date)), "%d %b %Y")
-  output$tripdate_s_dt_o<-renderText({tripdate_s_dt})
-  output$tripdate_e_dt_o<-renderText({tripdate_e_dt})
+    ##add group # to match sighting number in final data
+    sigminmax<-f_data%>%filter(!is.na(signum))%>%
+      group_by(signum)%>%
+      mutate(max = max(Datetime),
+             min = min(Datetime))%>%
+      distinct(DATE, signum, min, max)
+    
+    sigminmax$DATE<-ymd(sigminmax$DATE)
+    
+    allmerge_dt<-allmerge_dt%>%dplyr::select(-Group)
+    
+    allmerge_dt_group<-allmerge_dt%>%
+      left_join(sigminmax, by = c('Date' = 'DATE'))%>%
+      mutate(Group = case_when(
+        Datetime >= min & Datetime <= max ~ signum
+      ))%>%
+      filter(!is.na(Group))%>%
+      dplyr::select(-signum, -max, -min)%>%
+      distinct()
+    
+    nogroup<-allmerge_dt%>%
+      anti_join(allmerge_dt_group)
+    
+    allmerge_dt_group<-allmerge_dt_group%>%
+      bind_rows(nogroup)%>%
+      arrange(Datetime)
+    
+    write.csv(allmerge_dt_group, paste0(pathimage,"/Photo analysis/f_PA_",phyear,"_",phmonth,".csv"), row.names = F, na = "")  
+    
   recent<-format(min(as.Date(allmerge_dt$Date)) - lubridate::years(1), "%d %b %Y")
   older<-format(min(as.Date(allmerge_dt$Date)) - lubridate::years(2), "%d %b %Y")
-  
+
   ##################
   ## life history ##
   ##################
   
   lifehist<-read.csv('./data/FBD_lifehistory.csv', header = T, stringsAsFactors = F)
-
+  
   photo_counts<-allmerge_dt%>%
     filter(!grepl("\\?",ID_Name))%>%
     filter(ID_Name != "CALF")%>%
     group_by(Date, ID_Name)%>%
     tally()
   
+  print('259')
   daily_cap<-photo_counts%>%
     ungroup()%>%
     mutate_if(is.numeric, ~1 * (. > 0))%>%
@@ -257,11 +308,11 @@ observeEvent(input$photogo,{
     filter(NAME != "" & !grepl('_',NAME) & !grepl('CULL',NAME) & NAME != "UNMA" & !str_detect(NAME, "^UK") & !str_detect(NAME, "\\?"))%>%
     tidyr::pivot_wider(id_cols = c("NAME","SEX","BIRTH_YEAR","FIRST_YEAR"), names_from = "Date", values_from = "n")%>%
     arrange(NAME)%>%
-    mutate(year = phyear)%>%
+    mutate(year = as.numeric(phyear))%>%
     mutate(AgeClass = case_when(
-      year - FIRST_YEAR > 3 ~ 'A',
-      year - BIRTH_YEAR == 0 ~ 'C',
-      !is.na(BIRTH_YEAR) & year - BIRTH_YEAR <= 3 & year - BIRTH_YEAR >= 0 ~ 'J',
+      (year - FIRST_YEAR) > 3 ~ 'A',
+      (year - BIRTH_YEAR) < 1 ~ 'C',
+      BIRTH_YEAR != 0 & (year - BIRTH_YEAR) <= 3 & (year - BIRTH_YEAR) > 0 ~ 'J',
       TRUE ~ 'U'
     ))%>%
     mutate(SEX=replace(SEX, is.na(SEX), 'X'))
@@ -269,6 +320,7 @@ observeEvent(input$photogo,{
   trip_cap<-daily_cap%>%
     distinct(NAME,SEX,AgeClass)
   
+  print('277')
   age_sex_table<-trip_cap%>%
     dplyr::select(-NAME)%>%
     group_by(SEX, AgeClass)%>%
@@ -279,7 +331,8 @@ observeEvent(input$photogo,{
     mutate(AgeClass = case_when(
       AgeClass == 'A' ~ "Adult",
       AgeClass == 'J' ~ "Juvenile",
-      AgeClass == 'C' ~ "Calf*"
+      AgeClass == 'C' ~ "Calf*",
+      AgeClass == 'U' ~ "Unknown"
     ))
   
   if (!"M" %in% colnames(age_sex_table)){
@@ -300,7 +353,7 @@ observeEvent(input$photogo,{
     tidyr::pivot_wider(names_from = "DATE", values_from = value)
   
   disco<-function(x){
-    
+    x<-daily_cap
     freqcap<-x%>%
       dplyr::select(-NAME, -SEX, -BIRTH_YEAR,-FIRST_YEAR,-AgeClass, -year)%>%
       as.data.frame()
@@ -327,15 +380,23 @@ observeEvent(input$photogo,{
   
   disco_data<-disco(daily_cap)
   
+  onoffeffort$DATE<-ymd(onoffeffort$DATE)
+  
+  disco_data<-disco_data%>%
+    left_join(onoffeffort, by = c('date' = 'DATE'))%>%
+    dplyr::select(-min, -max)
+  
   disco_curve<-ggplot(disco_data, aes(x=date, y = disc))+
-    geom_col(mapping = aes(x = date, y = ni), alpha = 0.5)+
-    geom_point(alpha = 0.5, size = 3)+
     geom_line()+
+    geom_col(mapping = aes(x = date, y = ni, color = permit), alpha = 0.5)+
+    geom_point(alpha = 0.8, size = 3, mapping = aes(color = permit))+
     theme_bw()+
-    scale_colour_viridis_d(option = "plasma", name = '')+
+    scale_colour_viridis_d(option = 'plasma', end = 0.7)+
     xlab("")+
     ylab("Individuals per day & cumulative IDs")+
-    ylim(c(0,max(disco_data$disc)))
+    ylim(c(0,max(disco_data$disc)))+
+    theme(legend.position="bottom",
+          text = element_text(size = 10))
   
   ggsave(filename = 'disco_curve.png',disco_curve,device = 'png', './figures', dpi = 320, width = 120, height = 80, units = 'mm')
   
@@ -353,6 +414,7 @@ observeEvent(input$photogo,{
     mutate(Date = lubridate::ymd(Date))%>%
     filter(value != 0)
   
+  print('365')
   all_first_last<-caphist%>%
     tidyr::pivot_longer(!(c(Entry,NAME)),names_to = "Date")%>%
     filter(value != 0)%>%
@@ -366,10 +428,11 @@ observeEvent(input$photogo,{
     filter(!is.na(NAME))%>%
     arrange(NAME)%>%
     left_join(lifehist, by = "NAME")%>%
+    mutate(year = as.numeric(phyear))%>%
     mutate(AgeClass = case_when(
       year - FIRST_YEAR > 3 ~ "A",
-      year - BIRTH_YEAR == 0 | year - BIRTH_YEAR < 0 ~ "C",
-      !is.na(BIRTH_YEAR) & year - BIRTH_YEAR <= 3 & year - BIRTH_YEAR >= 0 ~ "J",
+      year - BIRTH_YEAR < 1 ~ "C",
+      !is.na(BIRTH_YEAR) & year - BIRTH_YEAR <= 3 & year - BIRTH_YEAR > 0 ~ "J",
       TRUE ~ "U"
     ))
   
@@ -383,6 +446,9 @@ observeEvent(input$photogo,{
       nchar(NAME) > 2 ~ paste0(str_to_title(NAME)," (",SEX,")"),
       nchar(NAME) <= 2 ~ paste0(NAME," (",SEX,")")))
   
+  unseen_names<-unseen_two_years$NAME
+  print("392")
+  
   unseen_table<-unseen_two_years%>%
     group_by(LAST_YEAR, SEX, AgeClass)%>%
     tally()%>%
@@ -392,7 +458,8 @@ observeEvent(input$photogo,{
     mutate(AgeClass = case_when(
       AgeClass == 'A' ~ "Adult",
       AgeClass == 'J' ~ "Juvenile",
-      AgeClass == 'C' ~ "Calf*"
+      AgeClass == 'C' ~ "Calf*",
+      AgeClass == 'U' ~ "Unknown*"
     ))
   
   if (!"M" %in% colnames(unseen_table)){
@@ -419,12 +486,14 @@ incProgress(4/5)
 
 effmap<-ggplot()+
   geom_polygon(NZ_coast, mapping = aes(long,lat,group = group), alpha = 0.8)+
-  geom_path(f_data%>%arrange(Datetime), mapping = aes(LON, LAT, group = DATE, color = DATE))+
+  geom_point(f_data%>%arrange(Datetime), mapping = aes(LON, LAT, group = DATE, color = DATE), size = 0.1)+
+  #geom_path(f_data%>%arrange(Datetime), mapping = aes(LON, LAT, group = DATE, color = DATE))+
   theme_bw()+
   scale_color_viridis_d(name = "Date")+
   xlab("Longitude")+
-  ylab("Latitude")+
-  theme(legend.position = "none")
+  ylab("")+
+  theme(legend.position = "none",
+        axis.text.y=element_blank())
 
 sigmap<-ggplot()+
   geom_polygon(NZ_coast, mapping = aes(long,lat,group = group), alpha = 0.8)+
@@ -454,7 +523,7 @@ sigmap<-ggplot()+
       coord_sf(xlim = c(min(f_data$LON),max(f_data$LON)), ylim = c(min(f_data$LAT),max(f_data$LAT)), crs = 4269)
   }
   
-  map<-ggpubr::ggarrange(sigmap, effmap, common.legend = T)
+  map<-ggpubr::ggarrange(sigmap, effmap, common.legend = T, legend = "bottom", widths = c(1.15,1), labels = 'auto')
   
   ggsave(filename = 'map.png',map,device = 'png', './figures', dpi = 320, width = 169, height = 120, units = 'mm')
   incProgress(5/5)
@@ -470,8 +539,8 @@ sigmap<-ggplot()+
       tempReport<-file.path("./scripts/FBD summary template.Rmd")
       file.copy("FBD summary template.Rmd", tempReport, overwrite = FALSE)
  
-      tripdate_s<-format(input$tripdate_s, "%d %b %Y")
-      tripdate_e<-format(input$tripdate_e, "%d %b %Y")
+      tripdate_s<-format(min(onoffeffort$DATE), "%d %b %Y")
+      tripdate_e<-format(max(onoffeffort$DATE), "%d %b %Y")
       loc_base<-paste0(pharea,"/",input$locbase)
       nsurveydays<-nrow(f_data%>%distinct(DATE))
       vessel<-input$vessel
@@ -491,7 +560,7 @@ sigmap<-ggplot()+
                     nsurveydays = nsurveydays, recent = recent, older = older, 
                     vessel = vessel, crew = crew, pop_est = pop_est, trip_cap = trip_cap,
                     track_dist = track_dist, sig_days = sig_days, sig_count = sig_count, hours_wTt = hours_wTt, 
-                    age_sex_table = age_sex_table, unseen_table = unseen_table,
+                    age_sex_table = age_sex_table, unseen_table = unseen_table, unseen_names = unseen_names,
                     wx_comments = wx_comments, calf_comments = calf_comments, next_comments = next_comments)
       print(params)
       rmarkdown::render(tempReport, output_file = file,
@@ -501,4 +570,5 @@ sigmap<-ggplot()+
   }
   })
 })
+  
   
