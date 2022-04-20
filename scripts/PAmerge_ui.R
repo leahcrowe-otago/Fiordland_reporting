c30ab9eb17608446b241fcea78059c79d5a5d07c
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2022-04-20 16:28:10 -0400

    update local pathway example, comment wording

--- scripts/PAmerge_ui.R
+++ scripts/PAmerge_ui.R
@@ -4,10 +4,11 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
       radioButtons("filepathway", "File Pathway", choices = c("Network", "Local"), selected = "Network", inline = FALSE), 
-      textInput("filepathinput", (HTML(paste("Local pathway for photo analysis spreadsheet", '<br/>', "Example: C:/Users/leah.crowe/Desktop/Canada Data Processing/")))),
+      textInput("filepathinput", (HTML(paste("Local pathway for photo analysis spreadsheet", '<br/>', "Example: C:/Users/leahm/Documents/Otago/FBD data management/Fieldwork/")))),
       textInput("photoyear","Year (YYYY)"),
       textInput("photomonth","Month (MM)"),
       radioButtons("areainput", "Area monitored", choices = c("Doubtful","Dusky","Milford","Other"), selected = "Doubtful", inline = FALSE), 
+      textInput("otherfiord","Other fiord inc. 'Sound'/'Inlet'"),
       radioButtons("EXIF", "collect EXIF data, or load file?", choices = c("collect","load"), selected = "load", inline = FALSE),
       actionButton("photogo", "Go"),
       br(),
@@ -19,11 +20,11 @@ fluidPage(
       uiOutput("crew"),
       textAreaInput("wx_comments","Weather comments:", "General comments on the weather during this trip", height = 100, width = 500), 
       textAreaInput("calf_comments","Calf comments:", "Highlight new calves observed in this trip", height = 100, width = 500),
-      textAreaInput("next_comments","Next step comments:", "Recommendations for the future generated from the experiences of this trip", height = 100, width = 500),
+      textAreaInput("next_comments","Next step comments:", "All individuals sighted were members of the x pod. Recommendations for the future generated from the experiences of this trip", height = 100, width = 500),
       br(),
       downloadButton("report", "Generate Report"),
       #textOutput("finalmess"),
-      br(),
+      br()
       #leafletOutput("finalleaf")
     )
   )
