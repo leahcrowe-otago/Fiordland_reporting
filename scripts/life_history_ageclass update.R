deee6ef05207ed6e6ae35d9330698d3be03e70e8
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2022-07-21 19:17:04 +1200

    W to J

--- scripts/life_history_ageclass update.R
+++ scripts/life_history_ageclass update.R
@@ -1,6 +1,6 @@
 library(odbc);library(dplyr);library(DBI);library(lubridate)
 
-source('./scripts/connect to MySQL.R', local = TRUE)$value
+source('~/Documents/git_otago/Fiordland_reporting/scripts/connect to MySQL.R', local = TRUE)$value
 #lifehist<-dbReadTable(con, "age_sex")
 
 assign_ageclass<-function(x){
@@ -13,7 +13,7 @@ assign_ageclass<-function(x){
   this_year_ageclass = case_when(
     (this_year_age >= 9 | this_year_age_est >= 8 | phyear >= FIRST_CALF) ~ 'A',
     (this_year_age < 9 & this_year_age >= 3) ~ 'S-A',
-    (this_year_age < 3 & this_year_age > 0) ~ 'W',
+    (this_year_age < 3 & this_year_age > 0) ~ 'J',
     this_year_age == 0 ~ 'C',
     TRUE ~ 'U'))%>%
   mutate(this_year_ageclass = case_when(
@@ -52,6 +52,10 @@ lifehist<-lifehist%>%
   mutate(LAST_YEAR = case_when(
     !is.na(DEATH_YEAR) ~ as.character(DEATH_YEAR),
     TRUE ~ LAST_YEAR
+  ))%>%
+  mutate(LAST_DATE = case_when(
+    NAME == "D-20110328" ~ "2011-03-28",
+    TRUE ~ LAST_DATE
   ))%>%
   dplyr::select(POD, NAME, CODE, SEX, MOM, FIRST_CALF, BIRTH_YEAR, FIRST_YEAR, FIRST_DATE, DEATH_YEAR, LAST_YEAR, LAST_DATE, this_year_ageclass)
 
