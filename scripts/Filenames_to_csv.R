f2cf0498b34512a005049cce3e0d42cc4c624272
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2024-09-30 17:18:22 -0400

    cleanup for thesis

--- scripts/Filenames_to_csv.R
+++ scripts/Filenames_to_csv.R
@@ -1,33 +1,50 @@
+# Data from filenames into tabular format
+# L. Crowe
+
+# libraries ----
 library(dplyr); library(exifr); library(stringr); library(lubridate)
 
 #attempt to fix premature eof error
 #configure_exiftool(command = character(0), install_url = TRUE)
 
+#1. specify survey area ----
 #survey_area<-'Doubtful'
 survey_area<-'Dusky'
 #survey_area<-'Dagg Sound'
+
+#2. specify year ----
+year<-2006
+
+#3. specify month ----
+month<-'10'
+
+#4. specify write location for output file ----
+write_location<-list.files(paste0(path,year,"/"))
+
+#5. run the code below ----
+# read comments as you go
 if (survey_area == 'Doubtful' | survey_area == 'Dusky'){
   fiord_path<-paste0(survey_area," Sound Dolphin Monitoring/")
 } else {
   fiord_path<-paste0('Other Fiords/',survey_area,'/')
 }
 path<-paste0("//storage.hcs-p01.otago.ac.nz/sci-marine-mammal-backup/Fiordland Bottlenose dolphin/Long Term Monitoring/",fiord_path)
-year<-2007
 list.files(paste0(path,year,"/"))
-month<-'12'
 
 fullpath<-paste0(path,year,"/",year,"_",month,"/")
 folder.list<-list.files(fullpath, pattern = paste0("^",year), full.names = F)
-#folder.list<-folder.list[5]
+folder.list<-list.files(fullpath, full.names = F)
+
 filenames<-sapply(folder.list, function (x) list.files(paste0(fullpath, x), pattern = c("*.jpg$|*.JPG$|*.NEF"), full.names = T, recursive = T))
 filenames_unlist<-unlist(filenames, use.names = F)
 length(filenames_unlist)
 
 print(Sys.time())
+#grabbing timestamp from image metadata can take a few hours depending on number of images and connection speed
 metadata<-exifr::read_exif(filenames_unlist, tags = c("filename", "DateTimeOriginal"))
 print(Sys.time())
 nrow(metadata)
-############
+
 ##this finds ands
 and<-metadata%>%
   mutate(FileName = str_replace(toupper(FileName), "WITH","and"))%>%
@@ -37,7 +54,10 @@ and<-metadata%>%
          Name = toupper(stringr::word(FileName, 3)),
          Comments = "")
   
-##### after Feb 2008
+
+## read carefully ----
+
+### after Feb 2008 ----
 photoperind<-metadata%>%
   mutate(FileName = str_replace(FileName, " '",""),
          Name = toupper(sub(" .*","",FileName)),
@@ -54,7 +74,6 @@ photoperind<-metadata%>%
            Part == 'r' ~ 'RD',
            TRUE ~ Part
          ))%>%
-  #dplyr::select(Filename, Date, DateTime, Name, Part, Comments)%>%
   mutate(SURVEY_AREA = toupper(survey_area),
          TRIP = paste0(year,"_",month), 
          Group = '',
@@ -64,7 +83,7 @@ photoperind<-metadata%>%
 
 head(photoperind)
 
-##### for 2008 and previous where code leads the filename
+### for 2008 and previous where ID CODE leads the filename ----
 photoperind<-metadata%>%
   mutate(Name = stringr::str_sub(FileName, 7,nchar(FileName)))%>%
   mutate(FileName = str_replace(FileName, " '",""),
@@ -82,7 +101,6 @@ photoperind<-metadata%>%
            Part == toupper('R') ~ 'RD',
            TRUE ~ Part
          ))%>%
-  #dplyr::select(Filename, Date, DateTime, Name, Part, Comments)%>%
   mutate(SURVEY_AREA = toupper(survey_area),
          TRIP = paste0(year,"_",month), 
          Group = '',
@@ -90,47 +108,70 @@ photoperind<-metadata%>%
   dplyr::select(SURVEY_AREA, TRIP, Date, Filename, DateTime, Name, Part, Group, Photographer, Comments, SourceFile, FileName, DateTimeOriginal)%>%
   arrange(DateTime, Filename)
 
+### for 2006 ----
+photoperind<-metadata%>% 
+  mutate(Name=toupper(str_match(SourceFile, '([^/]+)(?:/[^/]+){1}$')[,2]),
+         Filename = basename(SourceFile))%>%
+  mutate(Name = toupper(sub(" .*","",Name)),
+         Comments = case_when(
+           grepl("'", FileName) ~ "Conditional match",
+           TRUE ~ ''))%>%
+  bind_rows(and)%>%
+  mutate(Part = str_sub(FileName,-6,-6),
+         #Filename = str_sub(FileName,-12, -5),
+         DateTime = ymd_hms(DateTimeOriginal),
+         Date = as.Date(ymd_hms(DateTimeOriginal)),
+         Part = case_when(
+           Part == toupper('L') ~ 'LD',
+           Part == toupper('R') ~ 'RD',
+           TRUE ~ Part
+         ))%>%
+  mutate(SURVEY_AREA = toupper(survey_area),
+         TRIP = paste0(year,"_",month), 
+         Group = '',
+         Photographer = '')%>%
+  dplyr::select(SURVEY_AREA, TRIP, Date, Filename, DateTime, Name, Part, Group, Photographer, Comments, SourceFile, FileName, DateTimeOriginal)%>%
+  arrange(DateTime, Filename)
+
+#6. write output file ----
+
 head(photoperind)
 
-write.csv(photoperind, paste0('./data/Ind_per_photo_',year,"_",month,'_',Sys.Date(),'.csv'), row.names = F)
+write.csv(photoperind, write_location, row.names = F)
 
-#############
-# merge exif data done in segments for a trip #####
+#7. special cases ----
+## merge exif data done in segments for a trip ----
 
-csv_list<-list.files("./data", pattern = "Ind_per_photo_2016_12_2023-10-*", full.names = T, recursive = T)
+# csv_list<-list.files("./data", pattern = "Ind_per_photo_2016_12_2023-10-*", full.names = T, recursive = T)
+# 
+# csv_data<-lapply(csv_list, function(x)
+#   read.csv(x)
+# )
+# 
+# nrow(csv_data[[2]])
+# new_csv<-bind_rows(csv_data)
+# nrow(new_csv)
+# unique(new_csv$Date)
+# write.csv(new_csv, paste0('./data/Ind_per_photo_',year,"_",month,'_',Sys.Date(),'_merge.csv'), row.names = F)
 
-csv_data<-lapply(csv_list, function(x)
-  read.csv(x)
-)
-
-nrow(csv_data[[2]])
-new_csv<-bind_rows(csv_data)
-nrow(new_csv)
-unique(new_csv$Date)
-write.csv(new_csv, paste0('./data/Ind_per_photo_',year,"_",month,'_',Sys.Date(),'_merge.csv'), row.names = F)
-
-# del duped photos from network ####
-# cleanup
-
+## del duped photos from network ----
 #read in slightly processed csv, before checking of multiple animals in photos, etc, but deleted the photo file names from the "Name" column
-csv_list<-list.files("./data", pattern = "Ind_per_photo_2016_04_2023-10-*", full.names = T, recursive = T)
-csv_data<-read.csv(csv_list[1])
-head(csv_data)
-nrow(csv_data)
 
-dupe_photos_del<-csv_data%>%
-  group_by(DateTime, Filename)%>%
-  mutate(n = n())%>%
-  filter(n > 1 & Name == "")
+# csv_list<-list.files("./data", pattern = "Ind_per_photo_2016_04_2023-10-*", full.names = T, recursive = T)
+# csv_data<-read.csv(csv_list[1])
+# head(csv_data)
+# nrow(csv_data)
+# 
+# dupe_photos_del<-csv_data%>%
+#   group_by(DateTime, Filename)%>%
+#   mutate(n = n())%>%
+#   filter(n > 1 & Name == "")
+# 
+# unique_records<-csv_data%>%
+#   group_by(DateTime, Filename)%>%
+#   mutate(n = n())%>%
+#   filter(n == 1 | Name != "")
+# nrow(unique_records)
+# 
+# write.csv(unique_records, paste0('./data/Ind_per_photo_2016_04_2023-10-17_nodupes.csv'), row.names = F)
 
-unique_records<-csv_data%>%
-  group_by(DateTime, Filename)%>%
-  mutate(n = n())%>%
-  filter(n == 1 | Name != "")
-nrow(unique_records)
-
-write.csv(unique_records, paste0('./data/Ind_per_photo_2016_04_2023-10-17_nodupes.csv'), row.names = F)
-
-4484+696
-
-#lapply(dupe_photos_del$SourceFile[7:695], function(x) file.remove(x))
