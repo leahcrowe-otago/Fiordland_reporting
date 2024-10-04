dcc3cefb963d79d34e8fb83d84f9e768628c193d
leahcrowe-otago <leah.crowe@postgrad.otago.ac.nz>
2024-10-03 20:50:21 -0400

    PA to data_merge

--- app.R
+++ app.R
@@ -13,15 +13,15 @@ ui <- dashboardPage(
   #sidebar content
   dashboardSidebar(
     sidebarMenu(
-      menuItem(icon = icon("camera"),"PA merge", tabName = "PAmerge")#,
+      menuItem(icon = icon("camera"),"Data merge", tabName = "Data_merge")#,
       #menuItem(icon = icon("whale"),"IWC", tabName = "IWC")
     )                                     
   ),
   dashboardBody(
     
   tabItems(  
-  tabItem(tabName = "PAmerge",
-          source('./scripts/PAmerge_app.R', local = TRUE)$value)#,
+  tabItem(tabName = "Data_merge",
+          source('./scripts/data_merge_app.R', local = TRUE)$value)#,
   #tabItem(tabName = "IWC",
    #       source('./scripts/IWC_app.R', local = TRUE)$value)
   
